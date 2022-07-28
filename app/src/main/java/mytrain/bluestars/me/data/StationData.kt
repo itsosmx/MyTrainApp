978c7ab03cc02415ef11d0eebe1b1f546c6ccee4
omnia1tarek <elgarhey1802@gmail.com>
2022-07-28 23:27:39 +0200

    ticket list

---
+++ app/src/main/java/mytrain/bluestars/me/data/StationData.kt
@@ -0,0 +1,14 @@
+package mytrain.bluestars.me.data
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class StationData (
+    var id:Long?=null,
+    var arrivalTime:String?=null,
+    var departureTime:String?=null,
+    var price:Double?=null,
+    var type:String?=null,
+    val isArrivalSameDate: Boolean = true,
+
+)
--- app/src/main/java/mytrain/bluestars/me/payment/TicketInfo.kt
+++ app/src/main/java/mytrain/bluestars/me/payment/TicketInfo.kt
@@ -7,13 +7,15 @@ import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.database.*
 import mytrain.bluestars.me.R
+import mytrain.bluestars.me.TicketsList
 
 
 class TicketInfo : AppCompatActivity() {
     private lateinit var tv_from: TextView
-    private lateinit var tv_to:TextView
+    private lateinit var tv_to: TextView
     private lateinit var b_next: Button
     private lateinit var myName: TextView
+    private lateinit var dbref: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ticket_info)
@@ -27,14 +29,14 @@ class TicketInfo : AppCompatActivity() {
         b_next = findViewById(R.id.b_next)
         tv_from = findViewById(R.id.tv_from)
         tv_to = findViewById(R.id.tv_to)
-        myName =findViewById((R.id.tv_egp))
+        myName = findViewById((R.id.tv_egp))
 
         val to = intent.getStringExtra("to")
         val from = intent.getStringExtra("from")
         //
-        val traveler_number =intent.getStringExtra("traveler_number")
-        val ticket_degree =intent.getStringExtra("ticket_degree")
-        val travel_time =intent.getStringExtra("travel_time")
+        val traveler_number = intent.getStringExtra("traveler_number")
+        val ticket_degree = intent.getStringExtra("ticket_degree")
+        val travel_time = intent.getStringExtra("travel_time")
 
 
         tv_from.text = from
@@ -43,30 +45,21 @@ class TicketInfo : AppCompatActivity() {
 
 
         b_next.setOnClickListener {
-            val intent = Intent(this@TicketInfo, PaymentMethod::class.java)
+            val intent = Intent(this@TicketInfo, TicketsList::class.java)
             intent.putExtra("from", from)
             intent.putExtra("to", to)
             //intent.putExtra("date",et_travel_date.)
             intent.putExtra("traveler_number", traveler_number)
-            intent.putExtra("ticket_degree",ticket_degree)
-            intent.putExtra("travel_time",travel_time)
+            intent.putExtra("ticket_degree", ticket_degree)
+            intent.putExtra("travel_time", travel_time)
             startActivity(intent)
 
 
         }
 
 
-
     }
-    inner class ReadDate: ValueEventListener{
-        override fun onDataChange(p0: DataSnapshot) {
-              var name :String =p0?.getValue(String::class.java)!!
-            myName.text = name
-        }
 
-        override fun onCancelled(error: DatabaseError) {
-            TODO("Not yet implemented")
-        }
 
-    }
 }
+
