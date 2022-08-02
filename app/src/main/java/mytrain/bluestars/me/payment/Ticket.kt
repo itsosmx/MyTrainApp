efc736fbd3b658476b569c73ad26322bd659251c
itsosmx <o.h1277@yahoo.com>
2022-08-02 16:01:21 +0200

    main

---
+++ app/src/main/java/mytrain/bluestars/me/payment/Ticket.kt
@@ -0,0 +1,11 @@
+package mytrain.bluestars.me.payment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ticket : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket)
+    }
+}
