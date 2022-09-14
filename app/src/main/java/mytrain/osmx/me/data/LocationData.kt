96ee470a1ac966aad401d0374a2b0ecbb9871172
Mostafa77Elwany <me01002774176@gmail.com>
2022-09-15 01:38:56 +0200

    map+qr generator

--- app/src/main/java/mytrain/osmx/me/data/LocationData.kt
+++ app/src/main/java/mytrain/osmx/me/data/LocationData.kt
@@ -1,2 +1,12 @@
-package mytrain.osmx.me 
+package mytrain.osmx.me.data
 
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+data class LocationData (
+
+    var f_latitude:Double? = null,
+    var f_longitude:Double? = null
+
+)
